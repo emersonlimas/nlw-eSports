2b5f13f99ee73d77a13d14a9f5e45ccf61c58923
emersonlimas <emersondelimasilva@hotmail.com>
2022-09-17 23:13:46 -0300

    Project create

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89deg, rgba(149,114,252,1) 16%, rgba(67,231,173,1) 40%, rgba(225,213,93,1) 97%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
+  },
+  plugins: [],
+}
